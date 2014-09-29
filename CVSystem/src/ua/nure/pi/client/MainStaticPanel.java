78e1caf33cc9430431c8cecd79ec23e735c5d7b1
Stas <stas249501@gmail.com>
2014-09-29 12:52:50 +0300

    static pages

---
+++ CVSystem/src/ua/nure/pi/client/MainStaticPanel.java
@@ -0,0 +1,11 @@
+package ua.nure.pi.client;
+
+public class MainStaticPanel extends StaticPanel {
+
+	private static String text = ""; 
+	
+	public MainStaticPanel() {
+		super(text);
+	}
+
+}
---
+++ CVSystem/src/ua/nure/pi/client/StaticPanel.java
@@ -0,0 +1,18 @@
+package ua.nure.pi.client;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+public class StaticPanel extends SimplePanel {
+	
+	protected HTML html;
+	
+	public StaticPanel() {
+		add(html);
+	}
+	
+	protected StaticPanel(String htm){
+		this();
+		html.setHTML(htm);
+	}
+}
