e01467182ce4dd2713d621664af0d1f446f7720e
Stas <stas249501@gmail.com>
2014-10-21 08:56:17 +0300

    забыл вчера отправить

---
+++ CVSystem/src/ua/nure/pi/client/PathClient.java
@@ -0,0 +1,8 @@
+package ua.nure.pi.client;
+
+public class PathClient {
+		
+	//static pages
+	public static final String STATIC_PAGE_MAIN = "/staticPages/main.html";
+	public static final String STATIC_PAGE_ABOUT = "/staticPages/about.html";
+}
